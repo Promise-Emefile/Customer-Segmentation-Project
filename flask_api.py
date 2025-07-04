cb3b9f397ac7a8dcfe802bfc1b9a135574f9aee1
Promise Emefile <64411597+Promise-Emefile@users.noreply.github.com>
2025-07-04 19:35:09 +0100

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -53,4 +53,4 @@ def predict():
 
 if _name_ == '_main_':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
