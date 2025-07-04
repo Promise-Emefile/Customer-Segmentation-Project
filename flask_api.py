1a9475dff6c46bbbb2cfca97241a784d0ef6dddc
Promise Emefile <64411597+Promise-Emefile@users.noreply.github.com>
2025-07-04 19:52:26 +0100

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -51,6 +51,6 @@ def predict():
 
     return data.to_html(classes='table table-striped')
 
-if _name_ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
